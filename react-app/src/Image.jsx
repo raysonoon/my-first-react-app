84f52d2de0173337a7f973e2e8f79bb029ba58e3
raysonoon <raysonoonhere@gmail.com>
2024-12-17 14:30:57 +0800

    Complete HTML to JSX conversion and curly braces in React

--- react-app/src/Image.jsx
+++ react-app/src/Image.jsx
@@ -1,7 +1,7 @@
 // No longer necessary to import React in JavaScript files since React v17.0
 
 function Image() { 
-    return <img src="../public/traditional-pandan-waffles-in-singapore.jpg" alt="waffles" height="200" width="200"></img>
-};
+    return <img src="/traditional-pandan-waffles-in-singapore.jpg" alt="waffles" height="200" width="200" />;
+}
 
 export default Image;
--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -4,11 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import Greeting from "./Greeting.jsx";
 import Image from "./Image.jsx";
+import HTMLtoJSX from "./htmltojsx.jsx";
+import HTMLtoJSX2 from "./htmltojsx2.jsx";
+import ToDoList from "./ToDoList.jsx";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting />
     <Image />
+    <HTMLtoJSX />
+    <HTMLtoJSX2 />
+    <ToDoList />
   </StrictMode>,
 );
